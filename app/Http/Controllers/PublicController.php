d7a0c0bb81b21b9118ca5f0b1f1bbcf9f0308a3f
YohanesAgengHZP <81618432+YohanesAgengHZP@users.noreply.github.com>
2023-01-03 14:16:37 +0700

    search on booklist added

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -3,13 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class PublicController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $books = Book::all();
-        return view('books-list',['books' => $books]);
+        $categories = Category::all();
+
+        if($request->category || $request->title){ //pencarian dengan category dan title
+           
+            $books = Book::where('title','like', '%'.$request->title.'%')
+            ->orWhereHas('categories', function($q) use ($request){
+                $q->where('categories.id', $request->category);
+            })->get();
+        
+        } else {
+            $books = Book::all();
+        };
+        return view('books-list',['books' => $books, 'categories' => $categories]);
     }
 }
--- resources/views/books-list.blade.php
+++ resources/views/books-list.blade.php
@@ -5,6 +5,27 @@
 @section('page-name', 'books')
 
 @section('content')
+
+    <form action="" method="get">
+        <div class="row row-search">
+            <div class="col-12 col-sm-6">
+                <select name="category" id="category" class="form-control">
+                    <option value="" aria-placeholder="">Search Book Category</option>
+                    @foreach ($categories as $item)
+                    <option value="{{ $item->id }}">{{ $item->name }}</option>
+                    @endforeach
+                </select>
+            </div>  
+
+            <div class="col-12 col-sm-6">
+                <div class="input-group mb-3">
+                    <input type="text" name="title" class="form-control" placeholder="Search Book Title" >
+                    <button class="btn btn-primary" type="submit">Search</button>
+                  </div>
+            </div>
+        </div>
+    </form>
+    
     <div class="my-5">
         <div class="row">
 
